f06c6f5f573ac9458f061c46c035f11c3b8172cc
yibin <rirboys@gmail.com>
2012-08-28 14:38:42 +0800

    add width and type options into simple alert

--- app/helpers/simple_alert_helper.rb
+++ app/helpers/simple_alert_helper.rb
@@ -3,8 +3,32 @@ module SimpleAlertHelper
 		@options = args.extract_options!
 		@meg = args.first
 		
+		return content_tag("div", :class => "alert-tag #{point} #{type}", :style => "#{width}") do 
+			alert_header + content_tag("div", "undisplay", :class => "alert-content")
+		end
+	end
+	
+	private
+	
+	def width
+		@options[:width] ||= 300
+		
+		if @options[:width].class == String
+			if @options[:width] =~ /^[0-9]+px$|^[0-9]+$/
+				@options[:width].delete('px').to_i
+			else
+				@options[:width] = 300
+			end
+		end
+	end
+	
+	def icon
 		@options[:icon] ||= :tag
 		
+		"alert-icon-#{@options[:icon].to_s.sub("_", "-")}"
+	end
+	
+	def point
 		@options[:point] ||= []
 		if @options[:point].include? :right
 			@options[:point].delete :left if @options[:point].include? :left
@@ -17,21 +41,19 @@ module SimpleAlertHelper
 			@options[:point].push :top unless @options[:point].include? :bottom
 		end
 		
-		return content_tag("div", :class => "alert-tag #{(@options[:point].collect { |p| 'alert-point-' + p.to_s }).join(" ")}") do 
-			alert_header + content_tag("div", "undisplay", :class => "alert-content")
-		end
+		(@options[:point].collect { |p| 'alert-point-' + p.to_s }).join(" ")
 	end
 	
-	private
-	
-	def alert
+	def type
+		@options[:type] ||= :default
 		
+		"alert-type-#{@options[:type]}"
 	end
 	
 	def alert_header
 		content_tag("div", :class => "alert-title") do
-			content_tag("span", "", :class => "alert-icon  alert-icon-#{@options[:icon]}") + 
-			content_tag("h4", "#{@options[:title]}", :style => "width:272px;") + 
+			content_tag("span", "", :class => "alert-icon  #{icon}") + 
+			content_tag("h4", "#{@options[:title]}", :style => "width:#{width-28}px;") + 
 			content_tag("a", "", :class => 'alert-icon alert-icon-remove alert-close')
 		end
 	end
